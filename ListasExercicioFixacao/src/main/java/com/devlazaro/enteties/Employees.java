3c26119c82a8db3837d45f88fc689d053c5f405a
LázaroK <lazarolk23@hotmail.com>
2025-07-10 22:31:38 -0300

    Práticando uso de List

---
+++ ListasExercicioFixacao/src/main/java/com/devlazaro/enteties/Employees.java
@@ -0,0 +1,53 @@
+package com.devlazaro.enteties;
+
+import java.util.Objects;
+
+public class Employees {
+
+    private long id;
+    private String name;
+    private double salary;
+
+    public Employees(long id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employees employees = (Employees) o;
+        return id == employees.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
